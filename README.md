89a891f1e45c9569fcc1b6ce846bc1fa9afa89b8
Matheus2212 <matheus.f.marques1998@gmail.com>
2022-02-05 10:14:32 -0300

    initial: First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Campaign URL Builder built in JS
+
+This script will generate the final URL of an ad after typing some info on the given input fields. 
+
+It also tries to fetch the website in an iframe for the user to check if its the correct website.
---
+++ campaign-url-builder.js
@@ -0,0 +1,35 @@
+var CAMPAIGNURLBUILDER = {
+        _inputs: null,
+        _iframe: null,
+        _final_url: null,
+        change(iterate) {
+                var t = this;
+                if (iterate == 0) {
+                        t._inputs[iterate].addEventListener("change", function () {
+                                var website = this.value;
+                                t._iframe.src = website;
+                        });
+                } else {
+                        t._inputs[iterate].addEventListener("keyup", function () {
+                                var website = t._inputs[0].value;
+                                var final_url = website;
+                                final_url += (t._inputs[1].value !== "" ? "?utm_source=" + encodeURIComponent(t._inputs[1].value) : "")
+                                final_url += (t._inputs[2].value !== "" ? "?utm_medium=" + encodeURIComponent(t._inputs[2].value) : "")
+                                final_url += (t._inputs[3].value !== "" ? "?utm_campaign=" + encodeURIComponent(t._inputs[3].value) : "")
+                                t._final_url.value = final_url;
+                        });
+                }
+        },
+        init(website_el, campaign_el, medium_el, source_el, iframe_el, final_url_el) {
+                this._inputs = [];
+                this._inputs.push(website_el);
+                this._inputs.push(source_el);
+                this._inputs.push(medium_el);
+                this._inputs.push(campaign_el);
+                this._iframe = iframe_el;
+                this._final_url = final_url_el;
+                for (var iterate = 0; iterate < this._inputs.length; iterate++) {
+                        this.change(iterate);
+                }
+        }
+}
